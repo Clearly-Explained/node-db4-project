459f266ee406ef51a230eee18cef0b175c4e8a19
Dionne <mylife33@hotmail.com>
2022-01-18 00:08:05 -0600

    migrations started

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-require('dotenv').config()
-const server = require("./api/server")
+const server = require('./api/server.js');
 
-const port = process.env.PORT
+const PORT = process.env.PORT || 9000;
 
-server.listen(port, () => console.log(`\nAPI running on port ${port}\n`))
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
